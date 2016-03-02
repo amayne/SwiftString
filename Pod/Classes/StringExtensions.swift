c60d3a0108eb150b409a0c7be1966cb162ee6ab6
Ray Lillywhite <raylillywhite@gmail.com>
2016-03-02 10:25:29 -0800

    Make camelize consistently use lower camel case

--- Pod/Classes/StringExtensions.swift
+++ Pod/Classes/StringExtensions.swift
@@ -28,12 +28,12 @@ public extension String {
     func camelize() -> String {
         let source = clean(" ", allOf: "-", "_")
         if source.characters.contains(" ") {
-            let first = source.substringToIndex(source.startIndex.advancedBy(1))
+            let first = source.substringToIndex(source.startIndex.advancedBy(1)).lowercaseString
             let cammel = NSString(format: "%@", (source as NSString).capitalizedString.stringByReplacingOccurrencesOfString(" ", withString: "", options: [], range: nil)) as String
             let rest = String(cammel.characters.dropFirst())
             return "\(first)\(rest)"
         } else {
-            let first = (source as NSString).lowercaseString.substringToIndex(source.startIndex.advancedBy(1))
+            let first = source.substringToIndex(source.startIndex.advancedBy(1)).lowercaseString
             let rest = String(source.characters.dropFirst())
             return "\(first)\(rest)"
         }
