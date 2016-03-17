2c88347c1a58f89d5f13bae960f10adb9da87d6a
Andrés Catalán <a@catalandr.es>
2016-03-16 22:58:57 -0400

    Wraps the code in an XC project+workspace

---
+++ SwiftString/SwiftString.h
@@ -0,0 +1,19 @@
+//
+//  SwiftString.h
+//  SwiftString
+//
+//  Created by Andrés Catalán on 2016–03–16.
+//  Copyright © 2016 Ayre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftString.
+FOUNDATION_EXPORT double SwiftStringVersionNumber;
+
+//! Project version string for SwiftString.
+FOUNDATION_EXPORT const unsigned char SwiftStringVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftString/PublicHeader.h>
+
+
